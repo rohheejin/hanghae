b95971c762f55aec51aae62f1abbeeb0dcaa6b55
RohHeeJin <ranin00421@gmail.com>
2022-10-13 21:10:33 +0900

    [Fix] 오타수정

--- test/src/App.js
+++ test/src/App.js
@@ -6,35 +6,35 @@ function App() {
     { id: 1, title: 'REACT를 배워봅시다' },
     { id: 2, title: 'useState를 활용합시다' },
   ]);
+  const [state, setState] = useState('');
 
-  const [state, setState] = useState({ title: '' });
+  // []:Array
+  // {title:'value'}:Object
+  // '':String
+  // 1:number
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setState({ ...state, [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setState(e.target.value);
   };
 
-  const handleClick = () => {
-    const obj = { id: data.length + 1, title: state.title };
-    setData([...data, obj]);
-    console.log({ title: state.title });
+  const onClick = () => {
+    const a = { id: data.length + 1, title: state };
+    setData([...data, a]);
   };
 
   return (
     <div className='App'>
       <div className='form-nav'>
-        <input type='text' name='title' value={state.title} onChange={handleChange} required />
-        <button onClick={handleClick}>추가하기</button>
+        <input type='text' name='title' required onChange={onChange} />
+        <button onClick={onClick}>추가하기</button>
       </div>
       <h1>Todo List</h1>
       <div className='form-list'>
-        {data.map((data) => {
-          return (
-            <div className='list-container' key={data.id}>
-              <div className='list-title'>{data.title}</div>
-            </div>
-          );
-        })}
+        {data.map((item) => (
+          <div className='list-container' key={item.id}>
+            <div className='list-title'>{item.title}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
